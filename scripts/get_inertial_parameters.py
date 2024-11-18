38a90930c876563ea0813bc1b4d079d91c0f8a24
Bill Finger <ur.narmak@gmail.com>
2024-11-18 22:30:08 +0300

    add peg and hole

--- scripts/get_inertial_parameters.py
+++ scripts/get_inertial_parameters.py
@@ -1,29 +1,24 @@
 import trimesh
 
-# Функция для расчета инерционных характеристик
+
 def calculate_inertia(file_path, density):
-    # Загрузка 3D-модели
     mesh = trimesh.load(file_path)
-    
-    # Вычисление объема
+
     volume = mesh.volume
     print(f"Volume: {volume} m^3")
 
-    # Масса = объем * плотность
     mass = volume * density
     print(f"Mass: {mass} kg")
 
-    # Вычисление центра масс
     center_of_mass = mesh.center_mass
     print(f"Center of mass: {center_of_mass}")
 
-    # Тензор инерции относительно центра масс
     inertia_tensor = mesh.moment_inertia
     print(f"Inertia Tensor (relative to center of mass):\n{inertia_tensor}")
 
     return mass, center_of_mass, inertia_tensor
 
-# Функция для генерации URDF тега с инерциальными характеристиками
+
 def generate_urdf_inertia_tag(mass, center_of_mass, inertia_tensor):
     inertia_urdf = f"""
     <inertial>
@@ -34,6 +29,7 @@ def generate_urdf_inertia_tag(mass, center_of_mass, inertia_tensor):
     </inertial>
     """
     return inertia_urdf
+
 
 def generate_sdf_inertia_tag(mass, center_of_mass, inertia_tensor):
     inertia_sdf = f"""
@@ -52,15 +48,15 @@ def generate_sdf_inertia_tag(mass, center_of_mass, inertia_tensor):
     """
     return inertia_sdf
 
-# Пример использования
+
 if __name__ == "__main__":
-    file_path = "rbs_assets_library/bishop/meshes/bishop.stl"
+    file_path = "/home/narmak/assembly/rbs_assets_library/rbs_assets_library/models/hole/hole/hole_visual.obj"
     density = 6.0
 
     mass, center_of_mass, inertia_tensor = calculate_inertia(file_path, density)
     urdf_inertia_tag = generate_urdf_inertia_tag(mass, center_of_mass, inertia_tensor)
     sdf_inertia_tag = generate_sdf_inertia_tag(mass, center_of_mass, inertia_tensor)
-    
+
     print("Generated URDF Inertia Tag:")
     print(urdf_inertia_tag)
 
